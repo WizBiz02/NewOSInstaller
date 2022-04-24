cc23cf77f073f1f9c726264d8d9709e1d02ad8b7
WizBiz02 <56945232+WizBiz02@users.noreply.github.com>
2022-04-24 16:17:01 -0400

    Update and rename myWingetInstall to Required.bat

---
+++ Required.bat
@@ -0,0 +1,16 @@
+winget install 7zip.7zip
+winget install Blenderfoundation.Blender
+winget install Discord.Discord
+winget install Microsoft.PowerToys
+winget install Microsoft.VisualStudioCode
+winget install Python.Python.3
+py get-pip.py
+winget install Valve.Steam
+winget install VideoLAN.VLC 
+winget install Inkscape.Inkscape
+winget install Google.Chrome
+winget install Google.BackupAndSync
+winget install Adobe.AdobeAcrobatReaderDC
+winget install Oracle.JavaRuntimeEnvironment
+winget install Lexikos.AutoHotkey
+winget install WinDirStat.WinDirStat
