6f691972952df3b9826663bf3e4b5a3f6ad28f8d
WizBiz02 <56945232+WizBiz02@users.noreply.github.com>
2022-05-01 18:34:45 -0400

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -7,3 +7,5 @@ sudo apt-get install anki
 sudo apt-get install vlc
 sudo apt-get install sagemath
 sudo apt-get install discord
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
