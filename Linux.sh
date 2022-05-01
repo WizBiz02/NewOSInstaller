0938f5030877c5c04d28b8fd2c0864b6564497e2
WizBiz02 <56945232+WizBiz02@users.noreply.github.com>
2022-05-01 18:17:02 -0400

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -1,6 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install inkscape
 sudo apt-get install krita
 sudo apt-get install gummi
 sudo apt-get install anki
 sudo apt-get install vlc
 sudo apt-get install sagemath
+sudo apt-get install discord
