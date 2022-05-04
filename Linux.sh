dae2150758cbaf7654eab15069594c510692145e
WizBiz02 <56945232+WizBiz02@users.noreply.github.com>
2022-05-03 23:47:33 -0400

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -1,5 +1,5 @@
 sudo apt-get update && sudo apt-get dist update && sudo apt-get upgrade
-sudo apt-get install inkscape krita gummi anki sagemath discord gnome-tweaks-y
+sudo apt-get install inkscape krita gummi anki sagemath discord gnome-tweaks gparted -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
