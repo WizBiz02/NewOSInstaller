e2c79052a827da8ea1854e4484f3af9521a33523
WizBiz02 <56945232+WizBiz02@users.noreply.github.com>
2022-05-10 17:43:36 -0400

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -1,8 +1,28 @@
+# Doing the Lord's work
 sudo apt update && sudo apt dist-upgrade && sudo apt upgrade
-sudo apt install inkscape krita gummi anki sagemath discord gnome-tweaks gparted -y
+
+#Getting the important things installed ASAP
+sudo apt install inkscape krita gummi anki sagemath gparted vlc obs-studio -y
+
+#Getting the fun things installed 
+sudo apt install steam atlauncher discord -y
+
+#DTE Specific things
+sudo apt install awesome gnome-tweaks -y
+
+#Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+
+#VSCode
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code
+
+#Dropbox
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+
+
+
+#one-liner wget -O - https://raw.githubusercontent.com/WizBiz02/NewOSInstaller/main/Linux.sh | bash
