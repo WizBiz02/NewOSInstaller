03e2a43c0c4df3c870d72c6691d63d5a28779f8c
WizBiz02 <56945232+WizBiz02@users.noreply.github.com>
2023-08-12 17:23:00 -0400

    Update Required.bat

--- Required.bat
+++ Required.bat
@@ -1,17 +1,46 @@
+::MustHaves
+winget install Mozilla.Firefox
 winget install 7zip.7zip
-winget install Blenderfoundation.Blender
 winget install Discord.Discord
-winget install Microsoft.PowerToys
-winget install Microsoft.VisualStudioCode
-winget install Python.Python.3
+winget install Python.Python.3.12
 py get-pip.py
-winget install Valve.Steam
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.PowerToys
+
 winget install VideoLAN.VLC 
-winget install Inkscape.Inkscape
 winget install Google.Chrome
 winget install Google.Drive
-winget install Adobe.AdobeAcrobatReaderDC
-winget install Oracle.JavaRuntimeEnvironment
-winget install Lexikos.AutoHotkey
-winget install Mozilla.Firefox
+winget install Adobe.Acrobat.Reader.64-bit
+winget install Obsidian.Obsidian
+
+::SystemTools
+winget install CPUID.HWMonitor
+winget install WinDirStat.WinDirStat
+winget install CrystalDevWorld.CrystalDiskMark
+
+::Creation Tools
+::winget install Audacity.Audacity
+::winget install Blenderfoundation.Blender
+winget install Inkscape.Inkscape
 winget install OBSProject.OSBStudio
+winget install KDE.Krita
+
+::Games
+::winget install Valve.Steam
+::winget install Atlauncher.Atlauncher
+::winget install GOG.Galaxy
+::winget install Parsec.Parsec
+::winget install Ultimaker.Cura
+::winget install DolphinEmu.DolphinEmu
+::winget install ElectronicArts.EADesktop
+::winget install EpicGames.EpicGamesLauncher
+::winget install PlayStation.PSNow
+
+::Coding
+::winget install Lexikos.AutoHotkey
+::winget install Oracle.JavaRuntimeEnvironment
+::winget install Notepad++.Notepad++
+::winget install RProject.R
+::winget install TeXstudio.TeXstudio
+::winget install RProject.Rtools
+::winget install ChristianSchenk.MiKTeX
